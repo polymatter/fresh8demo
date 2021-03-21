810ae745d9c8cf1dd72516a29fa162ad1bc1a332
polymatter <jlake.jobs@gmail.com>
2021-03-21 19:05:24 +0000

    test display

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import sampleData from './sampledata.json';
+import { User } from './types';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('UserTable', () => {
+
+  test('can see all the registered users in the system & each user has a name, email and location', () => {
+    render(<App />);
+    (sampleData as User[]).forEach(user => {
+      (['name', 'email', 'location'] as (keyof User)[]).forEach(property => {
+        const element = screen.getAllByText(new RegExp(user[property].replace('(','\\(').replace(')','\\)'),'i'))[0];
+        expect(element).toBeInTheDocument();
+      })
+    });
+  });
 });
